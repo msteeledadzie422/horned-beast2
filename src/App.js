f529352c66ed2725a02ce22547559943472a5173
Mandela Steele-Dadzie <msteeledadzie@gmail.com>
2022-10-18 23:15:03 -0600

    pushing working code

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Header from './Components/Header';
 import Main from './Components/Main';
 import Footer from './Components/Footer';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
     return (
