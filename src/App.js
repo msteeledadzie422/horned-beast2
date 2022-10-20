05933c91b9410d5db2a20d40cb4abbfc4c33d001
Mandela Steele-Dadzie <msteeledadzie@gmail.com>
2022-10-19 23:55:21 -0600

    pushing code for lab 3

--- src/App.js
+++ src/App.js
@@ -3,16 +3,46 @@ import Header from './Components/Header';
 import Main from './Components/Main';
 import Footer from './Components/Footer';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Component } from "react";
+import list from './data.json';
+import SelectedBeast from './Components/SelectedBeast'
 
-function App() {
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      currentTitle: null,
+      currentImg: null,
+      currentDescription: null,
+      showModal: false
+    }
+  }
+
+  handleClose = () => this.setState({showModal: false});
+
+  handleSelectedBeast = (beast) => {
+    this.setState({
+      currentTitle: beast.props.title, 
+      currentImg: beast.props.image_url, 
+      currentDescription: beast.props.description, 
+      showModal: true})
+  }
+  
+  render() {
     return (
       <div className="App">
         <Header />
-        <Main />
+        <SelectedBeast 
+          currentTitle={this.state.currentTitle} 
+          currentImg={this.state.currentImg} 
+          currentDescription={this.state.currentDescription} 
+          show={this.state.showModal}
+          close={this.handleClose} />
+        <Main handleSelectedBeast={this.handleSelectedBeast} beastList={list}/>
         <Footer />    
       </div>
     );
   }
-
+}
 
 export default App;
